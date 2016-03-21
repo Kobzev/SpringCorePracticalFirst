dd0727d3750b928c431f5238cb2955776cffb410
Kostiantyn Kobziev <kostiantyn_kobziev@epam.com>
2016-03-21 16:48:41 +0200

    exception handler

--- src/main/java/ua/kobzev/theatre/controller/ExceptionController.java
+++ src/main/java/ua/kobzev/theatre/controller/ExceptionController.java
@@ -1,6 +1,8 @@
 package ua.kobzev.theatre.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 /**
  * Created by kkobziev on 3/19/16.
@@ -8,10 +10,10 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 @ControllerAdvice
 public class ExceptionController {
 
-//    @ExceptionHandler(Exception.class)
-//    public String catchException(Model model){
-//        model.addAttribute("textError", "Ups! Something happen. Please try again");
-//
-//        return "error";
-//    }
+    @ExceptionHandler(Exception.class)
+    public String catchException(Model model){
+        model.addAttribute("textError", "Ups! Something happen. Please try again");
+
+        return "error";
+    }
 }
