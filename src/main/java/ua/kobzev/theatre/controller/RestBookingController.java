736a54fcb2a4f4806b6010d74c98e62427bd9e08
Kostiantyn Kobziev <kostiantyn_kobziev@epam.com>
2016-04-05 22:12:25 +0300

    add rest controllers

---
+++ src/main/java/ua/kobzev/theatre/controller/RestBookingController.java
@@ -0,0 +1,12 @@
+package ua.kobzev.theatre.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by kkobziev on 4/5/16.
+ */
+@RestController
+@RequestMapping("rest/tickets")
+public class RestBookingController {
+}
